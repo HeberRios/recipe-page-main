54417be89dfd987aa90285b86fc3dc6184694668
HeberRios <68972507+HeberRios@users.noreply.github.com>
2024-01-29 17:34:00 -0600

    Add project structure and files
    The project structure for this project was generated by Astro.
    
    All the necessary assets for the project were added to the public folder
    , this for have all this assets in a public folder just as Astro do with
    its project structure, also the input.css was added.
    
    And finally in the tailwind.config.mjs were added the fonts and colors
    to be used in the project.

---
+++ README.md
@@ -0,0 +1,3 @@
+# recipe-page-main
+
+This repository is for the creation of [this](https://www.frontendmentor.io/challenges/recipe-page-KiTsR8QQKm) challenge from frontendmentor.io, with HTML, CSS, Astro and Tailwind.
---
+++ tailwind.config.mjs
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                outfit: "Outfit",
+                young: "Young Serif",
+            },
+            colors: {
+                "mule-fawn": {
+                    50: "#fbf3f1",
+                    100: "#f6e4de",
+                    200: "#ecc8bc",
+                    300: "#e0a591",
+                    400: "#d38064",
+                    500: "#c96746",
+                    600: "#bb5b3b",
+                    700: "#9c4d32",
+                    800: "#864732",
+                    900: "#663828",
+                    950: "#361c14",
+                },
+                disco: {
+                    50: "#fcf3f9",
+                    100: "#f9eaf4",
+                    200: "#f5d5e9",
+                    300: "#eeb3d7",
+                    400: "#e383bc",
+                    500: "#d65ea1",
+                    600: "#c33f83",
+                    700: "#a82e69",
+                    800: "#8b2957",
+                    900: "#7b284f",
+                    950: "#46112a",
+                },
+            },
+        },
+    },
+    plugins: [],
+};
